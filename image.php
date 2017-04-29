d3833db5baac6517d935c02f9ec24dac5a0632e6
aniketpanda <aniket.panda@gmail.com>
2017-04-29 13:36:44 +0530

    Revert "first comit"
    This reverts commit 4d849eee72d678a5d3b3850dd079e08ca1ceb92f.

--- image.php
+++
@@ -1,7 +0,0 @@
-<?php
-extract($_GET);
-$type = 'image/jpeg';
-header('Content-Type:'.$type);
-header('Content-Length: ' . filesize($name));
-readfile($name);
-?>
--- update.php
+++
@@ -1,7 +0,0 @@
-<?php
-	extract($_GET);
-	$f=fopen("chat.txt","a") or die("Unable to open file!");
-	fwrite($f,$data."\n");
-	fclose($f);
-?>
-
